0634486a1268a687936e778d27b6b1fb9f6f1475
peter <peteroluwadayo2017@gmail.com>
2025-07-24 02:57:07 +0100

    Initial commit with MainActivity and layout

---
+++ app/src/main/java/com/example/fanzone/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.fanzone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fanzone.utils.BaseActivityBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
